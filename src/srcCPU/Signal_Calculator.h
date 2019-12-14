e601173d0c706ba138736fe1ea97407b216baf83
arame002 <37461438+arame002@users.noreply.github.com>
2019-12-14 01:25:47 -0800

    Update Signal_Calculator.h

--- src/srcCPU/Signal_Calculator.h
+++ src/srcCPU/Signal_Calculator.h
@@ -3,6 +3,6 @@
 #define Signal_Calculator_h
 #include "Tissue.hpp"
 
-int Signal_Calculator (vector< vector<double> > locX , vector< vector<double> > locY , vector<double > centX , vector<double > centY ) ;
+vector<double > Signal_Calculator (vector< vector<double> > locX , vector< vector<double> > locY , vector<double > centX , vector<double > centY ) ;
 
 #endif /* Signal_Calculator_h */
