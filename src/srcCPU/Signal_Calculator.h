5a3b714953709cd5d23018bac8de8ae14f1dba3e
arame002 <37461438+arame002@users.noreply.github.com>
2019-12-17 14:14:15 -0800

    Update Signal_Calculator.h

--- src/srcCPU/Signal_Calculator.h
+++ src/srcCPU/Signal_Calculator.h
@@ -2,6 +2,8 @@
 #ifndef Signal_Calculator_h
 #define Signal_Calculator_h
 
+using namespace std ;
+
 vector<double> Signal_Calculator (vector< vector<double> > locX , vector< vector<double> > locY , vector<double > centX , vector<double > centY ) ;
 
 #endif /* Signal_Calculator_h */
