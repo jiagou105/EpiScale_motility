87ec648dbd1d200dafd266ec768e1e11f7357c18
arame002 <37461438+arame002@users.noreply.github.com>
2020-01-30 15:29:45 -0800

    Update SceCells.cu

--- src/srcGPU/SceCells.cu
+++ src/srcGPU/SceCells.cu
@@ -1452,7 +1452,7 @@ if (firstTimeReadDpp) {
    	lastTimeExchange=lastTimeExchange+dt ;
 	cout << "last time exchange is " << lastTimeExchange << endl ; 
 	cout << "dt is " << dt << endl ;  
-   	double exchPeriod=1 ; 
+   	double exchPeriod=12 ; 
 	if ( lastTimeExchange>exchPeriod) {
 		lastTimeExchange=0 ; 
 		//vector<CVector> cellCentersHost ; 
