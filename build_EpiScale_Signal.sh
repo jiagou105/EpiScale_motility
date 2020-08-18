2cacfd4fff2af185a5c3446f8aa1337f086ffa15
arame002 <37461438+arame002@users.noreply.github.com>
2020-08-17 23:23:26 -0700

    Update build_EpiScale_Signal.sh

--- build_EpiScale_Signal.sh
+++ build_EpiScale_Signal.sh
@@ -5,13 +5,12 @@ set -ie
 if [[ -z $1 ]]; then echo -e "USAGE\n\t$0 BUILD_NAME" && exit 256; fi
 BUILD_NAME=$1
 
-module load slurm/16.05.4
-module load openmpi/2.0.1-slurm-16.05.4
-module load cmake
-module load cuda/9.1
-module load extra
-module load GCCcore/6.3.0
 
+module load cmake
+module load cuda/9.0
+module load gcc/6.3.0
+#module load extra
+#module load openmpi/2.0.1-slurm-16.05.4
 rm -rf $BUILD_NAME
 mkdir -p $BUILD_NAME && cd $BUILD_NAME
 
@@ -24,4 +23,3 @@ ENABLE_TESTS_COMPILATION=False \
 ../
 
 make -j 20
-
